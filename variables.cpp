b8d26febab256bfaebe9baeefc8c79425ec21af7
jiyaapanchall <panchaljiya790@gmail.com>
2026-06-17 17:43:47 +0530

    variables and data_types
    This program demonstrates the use of fundamental C++ data types including int, float, double, char, bool, and string, along with variable declaration and initialization.

---
+++ variables.cpp
@@ -0,0 +1,18 @@
+include <iostream>
+using namespace std;
+
+int main(){
+    int age=25;
+    char grade='A';
+    float PI=3.14;
+
+    bool isSafe=true;
+    double price=99.99;
+
+    cout<<"Age: "<<age<<endl;
+    cout<<"Grade: "<<grade<<endl;
+    cout<<"PI: "<<PI<<endl;
+    cout<<"Is Safe: "<<isSafe<<endl;
+    cout<<"Price: "<<price<<endl;
+    return 0;
+}

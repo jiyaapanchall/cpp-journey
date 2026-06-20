13db0fbcb11a93d03f25ff95661d84818dca05d8
jiyaapanchall <panchaljiya790@gmail.com>
2026-06-20 10:06:12 +0530

    largest of 2 numbers

---
+++ largest of 2 numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    if (num1 > num2) {
+        cout << num1 << " is larger.";
+    } else {
+        cout << num2 << " is larger.";
+    }
+
+    return 0;
+}

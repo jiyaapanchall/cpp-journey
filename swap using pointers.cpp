4d64f68415afd9d934e9d770cdfa88b25fcf8268
jiyaapanchall <panchaljiya790@gmail.com>
2026-07-05 20:46:35 +0530

    swap using pointers

---
+++ swap using pointers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void swapNumbers(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Before Swapping:" << endl;
+    cout << "num1 = " << num1 << endl;
+    cout << "num2 = " << num2 << endl;
+
+    swapNumbers(&num1, &num2);
+
+    cout << "\nAfter Swapping:" << endl;
+    cout << "num1 = " << num1 << endl;
+    cout << "num2 = " << num2 << endl;
+
+    return 0;
+}

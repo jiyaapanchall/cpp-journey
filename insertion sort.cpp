c4384ff395ba2b56cf947a10166f2a09abca10d0
jiyaapanchall <panchaljiya790@gmail.com>
2026-07-19 11:34:33 +0530

    insertion sort

---
+++ insertion sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Function for Insertion Sort
+void insertionSort(int arr[], int n) {
+
+    for (int i = 1; i < n; i++) {
+
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements greater than key
+        while (j >= 0 && arr[j] > key) {
+
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at correct position
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
